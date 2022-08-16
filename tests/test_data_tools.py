a9f1f3847ee2471b024f8287d1d362cf7c126bac
N000360920 <mdesmedt@auchan.fr>
2022-08-16 16:50:49 +0200

    for pytest

--- tests/test_data_tools.py
+++ tests/test_data_tools.py
@@ -1,14 +1,17 @@
 import pytest
-from conftests import *
+import json
+import values as val
 from tools.data_tools import *
 
+territory_list = json.dumps(val.territories)
+TERRITORY_JSON = json.loads(territory_list)
+
+@pytest.fixture
+def get_territory_data():
+    return [(TERRITORY_JSON,"EST",48.69586941510869,5.60984605583475),(TERRITORY_JSON,"ILE-DE-FRANCE",48.73474076615772,2.4805003703964954)]
 
 
 
-
-
-T   
-# @pytest.mark.parametrize('territories_json, chosen_territory_name, lat_expected,lon_expected',get_territory_data())
 def test_get_territory_lat_lon(get_territory_data):
     # with pytest.raises(AssertionError):
     for data in get_territory_data:
@@ -18,3 +21,8 @@ def test_get_territory_lat_lon(get_territory_data):
                 longitude = territory['longitude']
         assert latitude == data[2]
         assert longitude== data[3]
+
+
+a ="Une bonne couverture de test, supérieure à 80%, est signe d'un projet bien testé et auquel il est plus facile d'ajouter de nouvelles fonctionnalités."
+
+b = "pytest --cov=mapapp  tests/"
